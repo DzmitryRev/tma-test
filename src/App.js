0a92b77b403bb1e6faa98601ea920f26b7c2b248
Dzmitry Reuchanka <d.reuchanka@mail.astondevs.ru>
2025-07-19 15:44:37 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [input3, setInput3] = useState("");
+
+  const [storageItem, setStorageItem] = useState("");
+
+  const setItem = () => {
+    localStorage.setItem(input1, input2);
+  };
+
+  const getFromLs = (key) => {
+    setStorageItem(localStorage.getItem(key) || "");
+  };
+
+  const clean = () => {
+    setStorageItem("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      TEST LocalStorage
+      <div>
+        SET ITEM:
+        <div>
+          KEY:
+          <input
+            value={input1}
+            onChange={(e) => {
+              setInput1(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          VALUE:
+          <input
+            value={input2}
+            onChange={(e) => {
+              setInput2(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          GET FROM LS:
+          <input
+            value={input3}
+            onChange={(e) => {
+              setInput3(e.target.value);
+            }}
+          />
+        </div>
+        <button onClick={setItem}>SET ITEM</button>
+        <div>
+          <button
+            onClick={() => {
+              getFromLs(input3);
+            }}
+          >
+            GET ITEM FROM LS
+          </button>
+          <button onClick={clean}>CLEAN</button>
+        </div>
+        <div>{storageItem}</div>
+      </div>
     </div>
   );
 }
