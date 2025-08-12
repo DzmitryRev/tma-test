3da956aba0761f5ce5c7fcba89994475527a8aa7
Dzmitry Reuchanka <d.reuchanka@mail.astondevs.ru>
2025-08-12 20:05:17 +0300

    otp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import OtpInput from "react-otp-input";
 
 function App() {
- 
+  const [otp, setOtp] = useState("");
+
   return (
     <div className="App">
-      <input id="tel" name="tel" type="text" autoComplete="tel" inputMode="tel"/>
+      <input
+        id="tel"
+        name="tel"
+        type="text"
+        autoComplete="tel"
+        inputMode="tel"
+      />
+
+      <OtpInput
+        value={otp}
+        onChange={setOtp}
+        numInputs={4}
+        renderSeparator={<span>-</span>}
+        renderInput={(props) => <input {...props} />}
+      />
     </div>
   );
 }
