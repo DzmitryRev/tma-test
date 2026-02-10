71079313f964234c16e88f7661eca79b32c11b8a
Dzmitry <85966136+DzmitryRev@users.noreply.github.com>
2026-02-10 14:07:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ function App() {
   const [status, setStatus] = useState("Проверка...");
   const [inited, setInited] = useState(false);
 
-  useEffect(
-    () => {
+  useEffect(() => {
       if(inited) {
-           const webUrl = window.location.href;
-    openLink(webUrl);
+        const webUrl = window.location.href;
+        openLink(webUrl);
       }
-    }, [inited]
-  )
+    }, [inited])
 
   useEffect(() => {
     try {
@@ -24,57 +22,8 @@ function App() {
     }
   }, []);
 
-  const handleRedirect = () => {
-    const webUrl = window.location.href;
-
-    setStatus(`${openLink}`);
-    openLink(webUrl);
-//     if(window?.Telegram?.WebApp?.openLink) {
-//        setStatus("openLink");
-// window?.Telegram?.WebApp?.openLink(webUrl);
-//     } else if(window?.Telegram?.WebApp?.openTelegramLink) {
-//        setStatus("openTelegramLink");
-//       window?.Telegram?.WebApp?.openTelegramLink(webUrl);
-//     }
-    
-    // try {
-    //   window.location.href = webUrl;
-    //   setStatus("window.location.href установлен");
-    // } catch (error) {
-    //   setStatus(`Ошибка редиректа: ${error}`);
-    // }
-  };
-  
-  const checkIsTMA = async () => {
-    setStatus("Вызываем isTMA('complete')...");
-    
-    try {
-      const result = await isTMA('complete');
-      setStatus(`isTMA вернул: ${result}`);
-      
-      if (result) {
-        setStatus("В ТМА");
-      } else {
-        setStatus("Не в ТМА");
-      }
-    } catch (error) {
-      setStatus(`Ошибка проверки: ${error}`);
-    }
-  };
-
-  useEffect(() => {
-    checkIsTMA();
-  }, []);
-
   return (
     <div className="App">
-      <button 
-        onClick={handleRedirect} 
-        style={{ color: "white" }}
-      >
-        Открыть в браузере
-      </button>
-      
       <h3 style={{color: "white"}}>{status}</h3>
     </div>
   );
