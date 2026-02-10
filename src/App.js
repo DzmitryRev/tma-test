bef895ed65ff5c31238343be32a256d585172501
Dzmitry <85966136+DzmitryRev@users.noreply.github.com>
2026-02-10 17:08:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { isTMA, init, showPopup, openLink  } from "@telegram-apps/sdk-react";
+import { isTMA, init, showPopup, openLink, closeMiniApp  } from "@telegram-apps/sdk-react";
 import "./App.css";
 import { useState, useEffect } from "react";
 
@@ -16,7 +16,7 @@ function App() {
           message: "Custom message",
           buttons: [{text: "Перейти", id: "1", type: "ok"}],
           title: "Custom title",
-        }).then(res => {setStatus(res); openLink("https://google.com/");});
+        }).then(res => {setStatus(res); openLink("https://google.com/"); closeMiniApp();});
       }
       
   }, [inited]);
