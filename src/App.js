f302e4217e1dad41717f8e563d7740d4d0b3202c
Dzmitry <85966136+DzmitryRev@users.noreply.github.com>
2026-02-11 12:06:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { isTMA, init, openLink, showPopup  } from "@telegram-apps/sdk-react";
+import { isTMA, init, showPopup  } from "@telegram-apps/sdk-react";
 import "./App.css";
 import { useState, useEffect } from "react";
 
